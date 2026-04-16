53931d412201c7d3a68b4666a4c266c64f6299cf
huy <huyxauzai2601@gmail.com>
2026-04-16 22:58:14 +0700

    update

--- src/main/java/org/example/it210miniprojectsession09/repository/LabRoomRepository.java
+++ src/main/java/org/example/it210miniprojectsession09/repository/LabRoomRepository.java
@@ -1,21 +1,4 @@
 package org.example.it210miniprojectsession09.repository;
 
-import org.example.it210miniprojectsession09.model.entity.LabRoom;
-import org.springframework.stereotype.Repository;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Repository
 public class LabRoomRepository {
-    private static final List<LabRoom> rooms = new ArrayList<>();
-
-    static {
-        rooms.add(new LabRoom("1", "Phong Lab A", "Phong thi nghiem A voi thiet bi hien dai", 30));
-        rooms.add(new LabRoom("2", "Phong Lab B", "Phong thi nghiem B voi thiet bi day du", 25));
-    }
-
-    public List<LabRoom> findAll() {
-        return rooms;
-    }
 }
